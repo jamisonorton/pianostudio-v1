30640087f944f5c4248ae63b90aa95a28d8687d8
Jamison Orton <jorton4@byu.edu>
2025-04-09 09:37:55 -0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 
 export default async function HomePage() {
