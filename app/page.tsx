040ab2555a8e843b819f2ef9e909329451def67c
Jamison Orton <jorton4@byu.edu>
2025-04-09 09:37:08 -0600

    added the schema to the database

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const users = await prisma.user.findMany();
+
   return (
-    <div className="text-center container my-4 mx-auto">
-      <h1 className="text-3xl mb4">Home Page</h1>
-      <div className="flex gap-2 justify-center">
-        <Button>Sign In</Button>
-        <Button>Register</Button>
-      </div>
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
     </div>
+
+    // <div className="text-center container my-4 mx-auto">
+    //   <h1 className="text-3xl mb4">Home Page</h1>
+    //   <div className="flex gap-2 justify-center">
+    //     <Button>Sign In</Button>
+    //     <Button>Register</Button>
+
+    //   </div>
+    // </div>
   );
 }
