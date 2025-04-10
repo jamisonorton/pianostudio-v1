d540e1d1710dda01270afc6204f71e6eaf8c5db8
Jamison Orton <jorton4@byu.edu>
2025-04-10 10:57:27 -0600

    updated the compose and page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,13 @@
-// import { Button } from "@/components/ui/button";
-import prisma from "@/lib/prisma";
+import { Button } from "@/components/ui/button";
 
-export default async function HomePage() {
-  const users = await prisma.user.findMany();
-
+export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
-        Superblog
-      </h1>
-      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
-        {users.map((user) => (
-          <li key={user.id} className="mb-2">
-            {user.name}
-          </li>
-        ))}
-      </ol>
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb4">Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button>Sign In</Button>
+        <Button>Register</Button>
+      </div>
     </div>
-
-    // <div className="text-center container my-4 mx-auto">
-    //   <h1 className="text-3xl mb4">Home Page</h1>
-    //   <div className="flex gap-2 justify-center">
-    //     <Button>Sign In</Button>
-    //     <Button>Register</Button>
-
-    //   </div>
-    // </div>
   );
 }
